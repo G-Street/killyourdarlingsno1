49e0f91f6d97335aaeb5cd528a443408f5da0c94
Jake Ireland <jakewilliami@icloud.com>
2026-06-26 20:49:59 +1200

    feat: Finish handling input control for Player sprite

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,6 @@ fn setup(mut commands: Commands) {
 
     commands.spawn((
         Sprite::from_color(Color::srgb(0.25, 0.25, 0.55), Vec2::new(300.0, 200.0)),
-        // Transform::from_xyz(0., 0., 0.),
         Player {
             weight: 0.1,
             velocity: 0.0,
@@ -28,12 +27,9 @@ fn setup(mut commands: Commands) {
     ));
 }
 
-// TODO: this is not yet working, but I am trying Frankenstein examples from bevy
-fn controls(
-    time: Res<Time>,
-    input: Res<ButtonInput<KeyCode>>,
-    mut transform: Query<&mut Transform>,
-) {
+fn controls(input: Res<ButtonInput<KeyCode>>, query: Single<(&Player, &mut Transform)>) {
+    let (_player, mut transform) = query.into_inner();
+
     if input.pressed(KeyCode::ArrowLeft) {
         transform.translation.x -= 1.0;
     }
