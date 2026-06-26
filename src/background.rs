44ecef9a4bf47f324bc12a82e0d4cee6222f11ed
Jake Ireland <jakewilliami@icloud.com>
2026-06-27 00:53:53 +1200

    docs: Add TODO note for fixing background tiling limit bug

--- src/background.rs
+++ src/background.rs
@@ -12,6 +12,10 @@ pub fn parallax_plugin(app: &mut App) {
 // TODO: is it idiomatic to ship a whole bundle from a submodule, or is it best to construct
 //   this in the main module on setup?  I just didn't want to import all of the bevy parallax
 //   stuff again in main.rs.
+//
+// TODO: Why is this texture not repeated more than a few times?  I think it's to do with
+//   the scaling factor...
+//     <github.com/Gialale-Games/bevy_parallaxium/tree/f18b3524>
 pub fn camera_parallax_bundle() -> impl Bundle {
     (
         Camera2d,
