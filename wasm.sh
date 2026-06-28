1922c02eca75476e3bf787f41cd2f49ab41419c9
Jake Ireland <jakewilliami@icloud.com>
2026-06-28 15:51:06 +1200

    fix: Add assets to export before archiving project

--- wasm.sh
+++ wasm.sh
@@ -5,4 +5,5 @@ set -eux -o pipefail
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --out-dir ./export/main/ --target web ./target/wasm32-unknown-unknown/release/main.wasm
 cp -f ./index.html ./export/main/index.html
+cp -rf ./assets ./export/main/assets/
 zip -r ./export/main.zip ./export/main/
